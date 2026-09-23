8f8b455846a3683fb19e9071b77c68b10408946f
Shital Sapkota <shital.sapkota1@gmail.com>
2026-09-23 12:20:36 +0300

    added roles 'customer/farmer/admin'

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,11 +2,11 @@ import dotenv from 'dotenv';
 import { definePrismaConfig } from '@prisma/cli-engine';
 import { defineConfig as ormConfig } from '@prisma/orm-postgres/config';
 
-dotenv.config({ path: '.env.local' });
+dotenv.config({ path: '.env' });
 
 export default definePrismaConfig({
   orm: ormConfig({
-    contract: "./src/prisma/contract.prisma",
+    contract: './src/prisma/contract.prisma',
     db: {
       connection: process.env['DATABASE_URL']!,
     },
